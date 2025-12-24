d61f126a9e2723822e95f2f21c5a462968a639f8
李顺 <vipls021@163.com>
2025-12-24 11:25:52 +0800

    1. 更新底层日志细节，解决日志无法关闭问题 2. 更新依赖

--- Snet.Windows.Controls/XmlnsPrefix.cs
+++ Snet.Windows.Controls/XmlnsPrefix.cs
@@ -3,5 +3,6 @@
 [assembly: XmlnsDefinition("https://shunnet.top", "Snet.Windows.Controls.textbox")]
 [assembly: XmlnsDefinition("https://shunnet.top", "Snet.Windows.Controls.combobox")]
 [assembly: XmlnsDefinition("https://shunnet.top", "Snet.Windows.Controls.property")]
+[assembly: XmlnsDefinition("https://shunnet.top", "Snet.Windows.Controls.property.wpf")]
 [assembly: XmlnsDefinition("https://shunnet.top", "Snet.Windows.Controls.converter")]
 [assembly: XmlnsDefinition("https://shunnet.top", "Snet.Windows.Controls.ledgauge")]
